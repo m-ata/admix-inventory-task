6f4f911122da5042c29f8484e5b049bf309b96f2
m-ata <muhammad_ata@outlook.com>
2022-02-19 18:11:41 +0300

    style: app title and publisher

--- src/components/AppTitlePublisher/index.tsx
+++ src/components/AppTitlePublisher/index.tsx
@@ -1,19 +1,23 @@
 import React from 'react';
-import { Avatar, Typography } from 'antd';
+import { Avatar, Typography, Space } from 'antd';
 import { AppOutput }  from './../../interfaces/AppOutput';
 
 const { Title, Text } = Typography;
+
+import './index.css';
 
 const AppTitlePublisher = (props: AppOutput) => {
 
   const { title, googlePlayStoreInfo } = props;
 
   return (
-    <div>
+    <Space>
       <Avatar size={64} shape={'square'} icon={<img src={googlePlayStoreInfo?.icon} />} />
-      <Title level={5} > {title} </Title>
-      <Text> {googlePlayStoreInfo?.studio} </Text>
-    </div>
+      <Text>
+        <Title level={5} className={'app-title'}> {title} </Title>
+        <Text className={'app-publisher'} > {googlePlayStoreInfo?.studio} </Text>
+      </Text>
+    </Space>
   )
 }
 export default AppTitlePublisher;
