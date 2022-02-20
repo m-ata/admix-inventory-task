4cba153a435d92f2fa15a13555598ea131890f1a
m-ata <muhammad_ata@outlook.com>
2022-02-20 21:36:32 +0300

    fix: added EMPTY_IMAGE in case googlePlayStoreInfo and appStoreInfo both are null

--- src/constant/index.ts
+++ src/constant/index.ts
@@ -6,3 +6,4 @@ export const defaultRequest = {
 	filters: [] as IFilter[],
 	sorts: [] as ISort[]
 }
+export const EMPTY_IMAGE = 'https://ecdn.teacherspayteachers.com/thumbitem/Blank-Game-Board-3-4468982-1553533522/original-4468982-1.jpg'
