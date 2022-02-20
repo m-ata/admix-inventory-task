f2323f51ae920e6e31515f8dfebeb1e0638cfb50
m-ata <muhammad_ata@outlook.com>
2022-02-20 15:05:01 +0300

    style: basic edit app page design

--- src/pages/EditApp/index.tsx
+++ src/pages/EditApp/index.tsx
@@ -1,10 +1,81 @@
 import * as React from 'react';
+import { PageHeader, Button, Divider, Form, Input, Row, Col, Select } from 'antd';
+import './index.css';
 
 const EditApp = () => {
     return (
-        <h3>
-            Edit App
-        </h3>
+        <div>
+            <PageHeader
+                title="Inventory / Edit"
+                extra={[
+                    <Button key="1">Cancel</Button>,
+                    <Button key="2" type="primary"> Save </Button>
+                ]}
+            />
+            <div className={'form-layout'}>
+                <Divider />
+                <Form
+                    name="basic"
+                    initialValues={{ remember: true }}
+                    autoComplete="off"
+                    layout={'vertical'}
+                >
+                    <Row gutter={12}>
+                        <Col span={12}>
+                            <Form.Item
+                                label="App Title"
+                                name="title"
+                            >
+                                <Input />
+                            </Form.Item>
+                        </Col>
+                        <Col span={12}>
+                            <Form.Item
+                                label="Publisher Name"
+                                name="studio"
+                            >
+                                <Input />
+                            </Form.Item>
+                        </Col>
+                    </Row>
+                    <Row gutter={12}>
+                        <Col span={12}>
+                            <Form.Item
+                                label="App ID"
+                                name="_id"
+                            >
+                                <Input />
+                            </Form.Item>
+                        </Col>
+                        <Col span={12}>
+                            <Form.Item
+                                label="Tags"
+                                name="tags"
+                            >
+                                <Input />
+                            </Form.Item>
+                        </Col>
+                    </Row>
+                    <Row gutter={12}>
+                        <Col span={12}>
+                            <Form.Item
+                                label="Categories"
+                                name="genre"
+                            >
+                                <Select
+                                    placeholder="Choose Category"
+                                    allowClear
+                                >
+                                    <Select.Option value="Action">Action</Select.Option>
+                                    <Select.Option value="Adventure">Adventure</Select.Option>
+                                </Select>
+                            </Form.Item>
+                        </Col>
+                    </Row>
+                </Form>
+            </div>
+        </div>
+
     )
 }
 export default EditApp;
