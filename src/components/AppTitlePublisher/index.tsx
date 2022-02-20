542ba920158963acc29d1045a38a44f2e62ce6d6
m-ata <muhammad_ata@outlook.com>
2022-02-20 21:36:32 +0300

    fix: added EMPTY_IMAGE in case googlePlayStoreInfo and appStoreInfo both are null

--- src/components/AppTitlePublisher/index.tsx
+++ src/components/AppTitlePublisher/index.tsx
@@ -1,18 +1,21 @@
 import React from 'react';
 import { Avatar, Typography, Space } from 'antd';
 import { IAppOutput }  from './../../interfaces';
+import { EMPTY_IMAGE } from '../../constant';
+import './index.css';
 
 const { Title, Text } = Typography;
 
-import './index.css';
 
 const AppTitlePublisher = (props: IAppOutput) => {
 
   const { title, googlePlayStoreInfo, appStoreInfo } = props;
 
+  const iconLink = appStoreInfo ? appStoreInfo?.icon : (googlePlayStoreInfo ? googlePlayStoreInfo?.icon : EMPTY_IMAGE)
+
   return (
     <Space>
-      <Avatar size={64} shape={'square'} icon={<img src={appStoreInfo ? appStoreInfo?.icon : googlePlayStoreInfo?.icon} />} />
+      <Avatar size={64} shape={'square'} icon={<img src={iconLink} />} />
       <Text>
         <Title level={5} className={'app-title'}> {title} </Title>
         <Text className={'app-publisher'} > {appStoreInfo ? appStoreInfo?.studio : googlePlayStoreInfo?.studio} </Text>
