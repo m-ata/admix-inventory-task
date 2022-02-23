8969e32c363e6e174368b689553e926518efa998
m-ata <muhammad_ata@outlook.com>
2022-02-23 13:56:59 +0300

    fix: convertAvails utility round off fix

--- src/utils/convertAvails.ts
+++ src/utils/convertAvails.ts
@@ -1,13 +1,14 @@
 export const convertAvails = (labelValue: number) => {
+
         return Math.abs(Number(labelValue)) >= 1.0e+9
     
-        ? (Math.abs(Number(labelValue)) / 1.0e+9).toFixed(1) + "B"
+        ? Number((Math.abs(Number(labelValue)) / 1.0e+9).toFixed(1)) + "B"
         : Math.abs(Number(labelValue)) >= 1.0e+6
     
-        ? (Math.abs(Number(labelValue)) / 1.0e+6).toFixed(1) + "M"
+        ? Number((Math.abs(Number(labelValue)) / 1.0e+6).toFixed(1)) + "M"
         : Math.abs(Number(labelValue)) >= 1.0e+3
     
-        ? (Math.abs(Number(labelValue)) / 1.0e+3).toFixed(1) + "K"
+        ? Number((Math.abs(Number(labelValue)) / 1.0e+3).toFixed(1)) + "K"
     
         : Math.abs(Number(labelValue));
     
